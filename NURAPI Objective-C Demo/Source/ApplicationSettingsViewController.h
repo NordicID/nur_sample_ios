f23d6b655df79eb3aee2cfeeb1c42b2df53f8e19
Jan Ekholm <jan.ekholm@d-pointer.com>
2016-11-10 13:36:03 +0200

    New tab bar icons.

---
+++ NURAPI Objective-C Demo/Source/ApplicationSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ApplicationSettingsViewController.h
+//  Nordic ID RFID Demo
+//
+//  Created by Jan Ekholm on 10/11/2016.
+//  Copyright © 2016 Jan Ekholm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApplicationSettingsViewController : UITableViewController
+
+@end
