f090514de432c0d2c4599a58d1fa197dcf2951db
Jan Ekholm <jan.ekholm@d-pointer.com>
2017-10-05 16:06:35 +0300

    New quick guide contents.

---
+++ NURAPI Objective-C Demo/Source/FirmwareDownloader.h
@@ -0,0 +1,13 @@
+//
+//  FirmwareDownloader.h
+//  Nordic ID RFID Demo
+//
+//  Created by Jan Ekholm on 05/10/2017.
+//  Copyright © 2017 Jan Ekholm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirmwareDownloader : NSObject
+
+@end
---
+++ NURAPI Objective-C Demo/Source/FirmwareDownloader.m
@@ -0,0 +1,13 @@
+//
+//  FirmwareDownloader.m
+//  Nordic ID RFID Demo
+//
+//  Created by Jan Ekholm on 05/10/2017.
+//  Copyright © 2017 Jan Ekholm. All rights reserved.
+//
+
+#import "FirmwareDownloader.h"
+
+@implementation FirmwareDownloader
+
+@end
