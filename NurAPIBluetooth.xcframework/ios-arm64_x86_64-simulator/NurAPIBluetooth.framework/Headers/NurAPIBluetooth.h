24fcf0b4a9d209fb5cb937eeabec07df831ea0fb
Jan Ekholm <jan.ekholm@d-pointer.com>
2021-03-09 10:09:35 +0200

    A new XCFramework
    The NurAPI Bluetooth framework is now an XCFramework. It allows for different architectures to be more easily added to the framework and makes it easier to use in client code. Functionality is identical.
    
    The demo project is updated to use the new framework. Note the removed “Run script” build stage.

---
+++ NurAPIBluetooth.xcframework/ios-arm64_x86_64-simulator/NurAPIBluetooth.framework/Headers/NurAPIBluetooth.h
@@ -0,0 +1,13 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NurAPIBluetooth.
+FOUNDATION_EXPORT double NurAPIBluetoothVersionNumber;
+
+//! Project version string for NurAPIBluetooth.
+FOUNDATION_EXPORT const unsigned char NurAPIBluetoothVersionString[];
+
+#import <NurAPIBluetooth/NurAPIBluetoothVersion.h>
+#import <NurAPIBluetooth/Bluetooth.h>
+
+
